15255010d46df9747d4f2789e2856c5ea7868d79
Robert Fisher <robertfi@usau-robertfi-m.attlocal.net>
2020-05-10 13:12:17 -0500

    Playing with base32 encoding

--- test.cpp
+++ test.cpp
@@ -33,6 +33,47 @@ std::string base36_encode(std::string_view in)
     return bic::ntos(encoder, 36);
 }
 
+std::string tr(std::string_view in, std::string_view from, std::string_view to)
+{
+    std::string out;
+    out.reserve(in.size());
+    for (char c: in) {
+        auto pos = from.find(c);
+        if (from.npos != pos) {
+            out.push_back(to[pos]);
+        } else {
+            out.push_back(c);
+        }
+    }
+    return out;
+}
+
+//a      -> ME======
+//ab     -> MFRA====
+//abc    -> MFRGG===
+//abcd   -> MFRGGZA=
+//abcde  -> MFRGGZDF
+//abcdef -> MFRGGZDFMY======
+
+std::string base32_encode(std::string in)
+{
+    namespace bic = base_integer_conversion;
+    using namespace boost::multiprecision;
+    cpp_int encoder;
+    import_bits(encoder, in.begin(), in.end(), 8);
+    auto padding = 5 - ((in.size() * 8) % 5);
+    if ((padding > 0) && (padding < 5)) encoder <<= padding;
+    std::string out { bic::ntos(encoder, 32) };
+    padding = 8 - (out.size() % 8);
+    if ((padding > 0) and (padding < 8)) out += std::string(padding, '=');
+    //At this point we are in RFC 4648 base32hex?
+    //Convert to standard RFC 4648 alphabet.
+    out = tr(out,
+            "0123456789abcdefghijxlmnopqrstuv",
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567");
+    return out;
+}
+
 int main(int argc, const char** argv)
 {
     int plain_int { INT_MAX / 2 };
@@ -56,4 +97,12 @@ int main(int argc, const char** argv)
     char to_encode[] { "test" };
     std::string encoded { base36_encode(to_encode) };
     std::cout << to_encode << " -> " << encoded << '\n';
+    std::cout << to_encode << " -> " << base32_encode(to_encode) << '\n';
+    std::cout << "testy" << " -> " << base32_encode("testy") << '\n';
+    std::cout << base32_encode("a") << '\n';
+    std::cout << base32_encode("ab") << '\n';
+    std::cout << base32_encode("abc") << '\n';
+    std::cout << base32_encode("abcd") << '\n';
+    std::cout << base32_encode("abcde") << '\n';
+    std::cout << base32_encode("abcdef") << '\n';
 }
